3468e3c0b948a11cd9d18fcabb68823a5632cc6e
SpaceStation09 <rachel_chen0915@outlook.com>
2025-07-03 17:11:47 +0800

    fix RPC path for smithery

--- server.py
+++ server.py
@@ -68,4 +68,4 @@ def relate_account(
 	return str(data)
 
 if __name__ == "__main__":
-	mcp.run(transport="http", host="127.0.0.1", port=8000, path="/relate-account")
+	mcp.run(transport="http", host="127.0.0.1", port=8000, path="/mcp")
